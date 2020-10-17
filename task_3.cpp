c622898de3e2fa4731e2a5ddc2220d01644a73e1
vershinina618 <73005698+vershinina618@users.noreply.github.com>
2020-10-17 12:17:16 +0800

    Add files via upload

---
+++ task_3.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+int get_power(int number, int power)
+{
+    long result = 1;
+    for (int i = 0; i < power; i++)
+    {
+        result *= number;
+    }
+    return result;
+}
+int main()
+{
+    int number, power;
+    printf("Input a number: ");
+    scanf_s("%d", &number);
+    printf("Input a power: ");
+    scanf_s("%d", &power);
+    printf("%d", get_power(number, power));
+
+    return 0;
+}
