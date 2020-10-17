73b5e434cef8d08c6ab3215d7134229f69bbd67a
vershinina618 <73005698+vershinina618@users.noreply.github.com>
2020-10-17 11:43:46 +0800

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    for (int i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 != 0)
+        {
+            printf("Fizz\n");
+        }
+        if (i % 5 == 0 && i % 3 != 0)
+        {
+            printf("Buzz\n");
+        }
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("FizzBuzz\n");
+        }
+        if (i % 3 != 0 && i % 5 != 0)
+        {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+}
