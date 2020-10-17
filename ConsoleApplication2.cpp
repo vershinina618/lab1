2884750ad9c60c4a1b0984cb70ace592f09ef9af
vershinina618 <73005698+vershinina618@users.noreply.github.com>
2020-10-17 11:48:56 +0800

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+int is_simple(int number)
+{
+    if (number <= 2)
+    {
+        return 1;
+    }
+    for (int i = 2; i * i <= number; i++)
+    {
+        if (number % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+    int number;
+    scanf_s("%d", &number);
+    printf("%d", is_simple(number));
+
+    return 0;
+}
